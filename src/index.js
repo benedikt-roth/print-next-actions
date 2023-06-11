f170ffa061e7fdc4ff9e3c4db20a495efd5960ff
Benedikt Roth <benedikt@rossgardt.de>
2023-06-11 20:46:36 +0200

    Add due dates

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mustache = require('mustache');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
+const dayjs = require('dayjs');
 
 const {
     getTagNames,
@@ -12,6 +13,8 @@ const {
 
 const INPUT_FILE = '/Users/roth/Desktop/next_actions.json';
 const AUTOMATION_SCRIPT = './resources/extract_next_actions.scpt';
+
+const GENERATE_PDF = !process.argv.includes('--no-pdf');
 
 async function run() {
     console.log('Extract next actions from OmniFocus');
@@ -25,7 +28,7 @@ async function run() {
 
     const contexts = getTagNames(tasks);
     const template = (await fs.readFile('./resources/context.html')).toString();
-     const outDir = `/Users/roth/Desktop`;
+    const outDir = `/Users/roth/Desktop`;
 
     await fs.mkdir(outDir + `/pdf`, {recursive: true});
     await fs.mkdir(outDir + `/html`, {recursive: true});
@@ -33,17 +36,34 @@ async function run() {
     for (let i=0; i < contexts.length-1; i++) {
         const rendered = mustache.render(template, {
             context: contexts[i],
-            tasks: tasks.filter(item => item.tag.name === contexts[i]),
+            tasks: tasks
+                .filter(item => item.tag.name === contexts[i])
+                .sort((a, b) => a.metadata.section > b.metadata.section)
+                .sort((a, b) => new Date(b.task.effectiveDueDate) - new Date(a.task.effectiveDueDate))
+                .map(item => ({
+                    ...item,
+                    task: {
+                        ...item.task,
+                        effectiveDueDate: !!item.task.effectiveDueDate
+                            ? dayjs(item.task.effectiveDueDate).format('DD. MM YYYY')
+                            : null,
+                    },
+                })),
         });
         await fs.writeFile(`${outDir}/html/${contexts[i]}.html`, rendered);
-        await renderPDF(`${outDir}/html/${contexts[i]}.html`, `${outDir}/pdf/${contexts[i]}.pdf`);
+
+        if (GENERATE_PDF) {
+            await renderPDF(`${outDir}/html/${contexts[i]}.html`, `${outDir}/pdf/${contexts[i]}.pdf`);
+        }
     }
 
-    const pdfs = (await fs.readdir(outDir + '/pdf'))
-        .filter(file => path.extname(file) === '.pdf')
-        .map(file => `${outDir}/pdf/${file}`);
+    if (GENERATE_PDF) {
+        const pdfs = (await fs.readdir(outDir + '/pdf'))
+            .filter(file => path.extname(file) === '.pdf')
+            .map(file => `${outDir}/pdf/${file}`);
 
-    await mergePDFs(pdfs, outDir + `/pdf/_combined.pdf`)
+        await mergePDFs(pdfs, outDir + `/pdf/_combined.pdf`)
+    }
 }
 
 run();
